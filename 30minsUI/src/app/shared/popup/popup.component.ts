b9b40f764b3036e3a9abd91e8fcb86c11d95616f
mhkhan4 <mhkhan4@buffalo.edu>
2023-03-05 12:15:26 -0500

    updated UI

---
+++ 30minsUI/src/app/shared/popup/popup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-popup',
+  templateUrl:'./popup.component.html'
+})
+export class PopupComponent implements OnInit{
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+}
